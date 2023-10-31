b1f4329f5930259421a15c59166e5254b2e1f0e3
Ilson Nòbrega <hello@ilson.dev>
2023-10-30 22:44:24 -0300

    feat: adicionando o arquivo principal e a rota de teste

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/api/test', async (req, res) => {
+    const filmes = [
+        { id: 1, nome: 'Filme 1' },
+        { id: 2, nome: 'Filme 2' },
+    ];
+    res.status(200).json(filmes);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
