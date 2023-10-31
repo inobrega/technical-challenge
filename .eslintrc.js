082c60b457d0a90de1fb24428d53c395d39c2dda
Ilson Nòbrega <hello@ilson.dev>
2023-10-30 23:01:00 -0300

    feat: ajustando o projeto para usar módulos do ES6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error',
-  },
+    'prettier/prettier': 'error'
+  }
 };
