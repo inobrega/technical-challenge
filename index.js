429684ef492f15680260bd532c6a73063ac5e8f6
Ilson Nòbrega <hello@ilson.dev>
2023-10-30 22:51:50 -0300

    feat: adicionando nodemon, eslint e prettier ao projeto para garantir qualidade

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 app.get('/api/test', async (req, res) => {
-    const filmes = [
-        { id: 1, nome: 'Filme 1' },
-        { id: 2, nome: 'Filme 2' },
-    ];
-    res.status(200).json(filmes);
+  const filmes = [
+    { id: 1, nome: 'Filme 1' },
+    { id: 2, nome: 'Filme 2' },
+  ];
+  res.status(200).json(filmes);
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
